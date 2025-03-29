60a1b35206a82bbdf34c4cc7079472881aec12ec
tvvmvn <tvvmvn@gmail.com>
2025-03-29 15:56:43 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import githubPages from 'vite-plugin-github-pages'; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), githubPages()],
+  plugins: [react()],
   base: '/react-19-go/', // Adjust based on your deployment needs 
 })
